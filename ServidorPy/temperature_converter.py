48908e1ba89e089c32a66c5a5e3e5afc05a1fe92
Dimitri-Code56 <85880875+Dimitri-Code56@users.noreply.github.com>
2023-04-23 14:41:33 -0300

    Delete temperature_converter.py

--- ServidorPy/temperature_converter.py
+++
@@ -1,9 +0,0 @@
-import Pyro4
-
-@Pyro4.expose
-class TemperatureConverter:
-    def celsius_to_fahrenheit(self, celsius):
-        return celsius * 1.8 + 32
-
-    def fahrenheit_to_celsius(self, fahrenheit):
-        return (fahrenheit - 32) / 1.8
